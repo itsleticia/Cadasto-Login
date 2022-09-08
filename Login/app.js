a07a3715f86f84e0e746a1bfb3b50de3888b2e04
Letícia Lima <109635088+itsleticia@users.noreply.github.com>
2022-09-08 00:34:07 -0300

    Add files via upload

---
+++ Login/app.js
@@ -0,0 +1,7 @@
+const sign_in_password = document.getElementsById('password');
+const sign_in_password = document.getElementsById('submit-button');
+
+const li_lower_upper = document.querySelector('#lower-upper');
+const li_numbers = document.querySelector('#numbers');
+const li_special_characteres = document.querySelector('#special-characteres');
+const li_digits = document.querySelector('#digits');
